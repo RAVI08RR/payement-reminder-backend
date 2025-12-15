91f155a70374ff3fb0a0af6e1b34895ee857486e
Mohammed Mustafa Khan <53117363+MustafaKhan026@users.noreply.github.com>
2025-12-15 15:17:39 +0530

    cors issue fixed

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from fastapi import FastAPI
 from database import engine
 from models import Base
 from routes import users, invoices
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Payment Reminder Backend")
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # can use ["*"] for all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(invoices.router)
